f56b15f3bd00a3e8fb4c5e87a8bff3ab1782628c
Andrii Zaitsev <105815678+zaytsevandrii@users.noreply.github.com>
2023-04-06 13:32:09 +0100

    add connection with api server and client

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import './App.css'
+import {useMutation, useQuery} from "@apollo/client";
+import {GET_ALL_USERS, GET_ONE_USER} from "./query/user";
+import {CREATE_USER} from "./mutations/user";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const {data, loading, error, refetch} = useQuery(GET_ALL_USERS)
+    const {data:oneUser, loading: loadingOneUser} = useQuery(GET_ONE_USER, {
+        variables: {
+            id: 1
+        }
+    })
+    const [newUser] = useMutation(CREATE_USER)
+    const [users, setUsers] = useState([])
+    const [username, setUsername] = useState('')
+    const [age, setAge] = useState(0)
+
+    console.log(oneUser)
+
+    useEffect(() => {
+        if (!loading) {
+            setUsers(data.getAllUsers)
+        }
+    }, [data])
+
+    const addUser = (e) => {
+        e.preventDefault()
+        newUser({
+            variables: {
+                input: {
+                    username, age
+                }
+            }
+        }).then(({data}) => {
+            console.log(data)
+            setUsername('')
+            setAge(0)
+        })
+    }
+    const getAll = e => {
+        e.preventDefault()
+        refetch()
+    }
+
+    if (loading) {
+        return <h1>Loading...</h1>
+    }
+
+    return (
+        <div>
+            <form>
+                <input value={username} onChange={e => setUsername(e.target.value)} type="text"/>
+                <input value={age} onChange={e => setAge(e.target.value)} type="number"/>
+                <div className="btns">
+                    <button onClick={(e) => addUser(e)}>Создать</button>
+                    <button onClick={e => getAll(e)}>Получить</button>
+                </div>
+            </form>
+            <div>
+                {users.map(user =>
+                    <div className="user">{user.id}. {user.username} {user.age}</div>
+                )}
+            </div>
+        </div>
+    );
+};
 
 export default App;
